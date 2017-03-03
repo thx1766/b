f570ea616f5351b09f8e6aed9086f4dab7f49fec
Nathanial Schaffner <schaffna@Nathanials-MacBook-Pro.local>
2017-03-03 06:55:55 -0500

    basic ZeFrank style brainjunk

--- SimEmpire/main.c
+++ SimEmpire/main.c
@@ -7,9 +7,14 @@
 //
 
 #include <stdio.h>
-
+void about(){
+    printf("\nautomate billing projections\nemployee call-out vs billing projection\nmonte-carlo simulation\n");
+    printf("\nmodel billing interface i see, guess what jake and brian see, hadoop for storing results, simulate months or billing cycles the way shop visit tool stores driver events\n");
+    printf("\nvit-vi for machines with touchbar, RPA tie-in for RPA sysadmin work\n");
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    about();
     return 0;
 }
